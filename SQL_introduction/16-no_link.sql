0aaf27815349e5ecd4841210ae6a8f7d10c148d2
LucasNGRE <lucas12negre@gmail.com>
2024-06-18 08:52:41 +0200

    add a comment

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,4 @@
+-- Lists the number of records in the second_table for each score, ordered by the number of records in descending order.
 SELECT score, name
 FROM second_table
 WHERE name IS NOT NULL
